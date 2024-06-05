175bf2d38a6da340d4d96dfb17dc5f2d700f7250
Kai Chang <kaijchang@gmail.com>
2024-06-05 12:28:54 -0700

    fix short track data

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ EVENTS = {
     '200-metres': 'sprints',
     '400-metres': 'sprints',
     '800-metres': 'middlelong',
-    '1000-metres': 'middlelong',
     '1500-metres': 'middlelong',
     '3000-metres': 'middlelong',
     '3000-metres-steeplechase': 'middlelong',
@@ -35,14 +34,15 @@ AGE_CATEGORY = ['senior'] # 'u18', 'u20'
 
 BEST_RESULTS_ONLY = [True] # False
 
-TRACK_SIZE = ['regular'] # 'irregular', 'all'
+MAX_RESULTS_BY_COUNTRY = ['all'] # '1', '2', '3', '4', '5'
 
 REGION_TYPE = ['world']
 
 GENDER = ['men', 'women']
 
 def get_top_list(event, environment, gender, age_category, year, event_category, **kwargs):
-    url = f'https://worldathletics.org/records/toplists/{event_category}/{event}/{environment}/{gender}/{age_category}/{year}?{urlencode(kwargs)}'
+    event_suffix = '-short-track' if environment == 'indoor' else ''
+    url = f'https://worldathletics.org/records/toplists/{event_category}/{event + event_suffix}/all/{gender}/{age_category}/{year}?{urlencode(kwargs)}'
 
     print(url)
 
@@ -64,8 +64,8 @@ def get_top_list(event, environment, gender, age_category, year, event_category,
 
 HEADERS = ['Rank', 'Mark', 'Competitor', 'DOB', 'Nat', 'Pos', '', 'Venue', 'Date', 'ResultScore']
 
-for (year, event, age_category, best_results_only, track_size, region_type, gender) in itertools.product(YEARS, filter(lambda e: e != '100-metres' and e != '10000-metres', EVENTS.keys()), AGE_CATEGORY, BEST_RESULTS_ONLY, TRACK_SIZE, REGION_TYPE, GENDER):
-    path = f'data/indoor/{event}/{gender}/{year}-{age_category}-{best_results_only}-{track_size}-{region_type}.csv'
+for (year, event, age_category, best_results_only, region_type, gender, max_results_by_country) in itertools.product(YEARS, filter(lambda e: e != '100-metres' and e != '10000-metres' and e != '3000-metres-steeplechase', EVENTS.keys()), AGE_CATEGORY, BEST_RESULTS_ONLY, REGION_TYPE, GENDER, MAX_RESULTS_BY_COUNTRY):
+    path = f'data/indoor/{event}/{gender}/{year}-{age_category}-{best_results_only}-{max_results_by_country}-{region_type}.csv'
     if not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
     with open(path, 'w') as results_file:
@@ -75,15 +75,15 @@ for (year, event, age_category, best_results_only, track_size, region_type, gend
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = []
             for page in PAGES:
-                futures.append(executor.submit(get_top_list, event, 'indoor', gender, age_category, year, EVENTS[event], page=page, regionType=region_type, bestResultsOnly=best_results_only, oversizedTrack=track_size))
+                futures.append(executor.submit(get_top_list, event, 'indoor', gender, age_category, year, EVENTS[event], page=page, regionType=region_type, bestResultsOnly=best_results_only, ageCategory=age_category, maxResultsByCountry=max_results_by_country))
                         
             for future in futures:
                 results = future.result()
                 if results is not None:
                     writer.writerows(results)
 
-for (year, event, age_category, best_results_only, region_type, gender) in itertools.product(YEARS, filter(lambda e: e != '60-metres' and e != '3000-metres', EVENTS.keys()), AGE_CATEGORY, BEST_RESULTS_ONLY, REGION_TYPE, GENDER):
-    path = f'data/outdoor/{event}/{gender}/{year}-{age_category}-{best_results_only}-{region_type}.csv'
+for (year, event, age_category, best_results_only, region_type, gender, max_results_by_country) in itertools.product(YEARS, filter(lambda e: e != '60-metres' and e != '3000-metres', EVENTS.keys()), AGE_CATEGORY, BEST_RESULTS_ONLY, REGION_TYPE, GENDER, MAX_RESULTS_BY_COUNTRY):
+    path = f'data/outdoor/{event}/{gender}/{year}-{age_category}-{best_results_only}-{max_results_by_country}-{region_type}.csv'
     if not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
     with open(path, 'w') as results_file:
@@ -93,7 +93,7 @@ for (year, event, age_category, best_results_only, region_type, gender) in itert
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = []
             for page in PAGES:
-                futures.append(executor.submit(get_top_list, event, 'outdoor', gender, age_category, year, EVENTS[event], page=page, regionType=region_type, bestResultsOnly=best_results_only))
+                futures.append(executor.submit(get_top_list, event, 'outdoor', gender, age_category, year, EVENTS[event], page=page, regionType=region_type, bestResultsOnly=best_results_only, ageCategory=age_category, maxResultsByCountry=max_results_by_country))
                         
             for future in futures:
                 results = future.result()
